f9d326ae8768d13f59a2d1695015a7574e7abf4d
Mrr-Sandy <saurabhmasiyal345@gmail.com>
2024-07-14 17:15:24 +0000

    day 2 completed

---
+++ challenges/operators.js
@@ -0,0 +1,32 @@
+let a = 5;
+let b = 4;
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+//understanding the assignment operators 
+
+let c = 10;
+let d = 22;
+
+c += 1;
+d -= 1;
+
+console.log(c, d);
+
+
+//understanding comparison operators 
+
+console.log(c>d, b<a);
+console.log(a>=b);
+console.log(c<=d);
+
+
+//logical operators 
+
+console.log(a>b && b< a);
+console.log(b==a || b>4);
+console.log(!b);
---
+++ challenges/vd.js
@@ -0,0 +1,33 @@
+//deckaring a variabke using "var"
+
+var num = 123;
+console.log(num);
+
+let num2 = 231;
+console.log(num2);
+
+
+//activity 2
+const isloggedin = true;
+console.log(isloggedin);
+
+
+let numb = 12;
+console.log(typeof(numb));
+
+let name2 = "saurabh";
+console.log(typeof(name2))
+
+let istrue = false;
+console.log(typeof(istrue));
+
+const student = {
+    "name2":"saurabh",
+     roll:21
+}
+console.log(typeof(student));
+
+//RESINGING NEW VALUE TO LET 
+num = 5;
+console.log(num);
+
